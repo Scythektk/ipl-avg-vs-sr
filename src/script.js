9ab80b56850dc3095fdb975bfb0e0b98f31c4fdc
Scythektk <kartikechopra@gmail.com>
2021-04-19 13:12:34 +0530

    initial commit

---
+++ src/script.js
@@ -0,0 +1,63 @@
+const svg = d3.select('svg');
+
+const width = +svg.attr('width');
+const height = +svg.attr('height');
+
+const render = (data) => {
+    const xValue = (d) => d.Population; // Change to second column heading of csv
+    const yValue = (d) => d.Country; // Change to first column heading of csv
+    const margin = { top: 80, bottom: 20, right: 35, left: 160 };
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
+
+    const xScale = d3
+        .scaleLinear()
+        .domain([0, d3.max(data, xValue)])
+        .range([0, innerWidth]); // define scale for data
+
+    const yScale = d3
+        .scaleBand()
+        .domain(data.map(yValue))
+        .range([0, innerHeight])
+        .padding(0.1); // define width of bars (height for horizontal bar chart)
+
+    const g = svg
+        .append('g')
+        .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+    const xAxis = d3.axisBottom(xScale); // FIXME: tickFormat(format('.3s') is not working
+
+    g.append('g')
+        .call(d3.axisLeft(yScale))
+        .selectAll('.domain, .tick line')
+        .remove(); // country axis labels
+    g.append('g')
+        .call(xAxis)
+        .attr('transform', `translate(0, ${innerHeight})`)
+        .select('.domain')
+        .remove(); // country axis labels; // population axis labels
+
+    g.selectAll('rect')
+        .data(data)
+        .enter()
+        .append('rect')
+        .attr('y', (d) => yScale(yValue(d)))
+        .attr('width', (d) => xScale(xValue(d)))
+        .attr('height', yScale.bandwidth());
+
+    g.append('text')
+        .text('Top ten Countries by Population 2021');
+}; // function to render
+
+function barChart(file_name) {
+    d3.csv(`../data/${file_name}.csv`).then((data) => {
+        data.forEach((d) => {
+            d.Population = +d.Population * 1000;
+        });
+        render(data);
+    });
+}
+
+barChart('population'); // Enter filename without extension
+
+// To load different csv go to lines 7, 8, 45 and 51
